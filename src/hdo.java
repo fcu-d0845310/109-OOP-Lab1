905dec916619a2f3f8a0292dab8f154d574f1db4
user <user@EE08>
2020-09-29 09:43:20 +0800

    1

---
+++ src/hdo.java
@@ -0,0 +1,10 @@
+
+public class hdo {
+	
+	public static void main(String[] args) {
+		System.out.println("i'm yuhuang");
+		System.out.println("My nid is D0845310");
+		System.out.println("This is lab1 HW");
+	}
+
+}
